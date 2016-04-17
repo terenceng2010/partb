b6775a3c4113050e9035181e862154aef8332191
terenceng2010 <10015485@life.hkbu.edu.hk>
2016-04-17 20:31:42 +0800

    first version of partb

---
+++ index.js
@@ -0,0 +1,30 @@
+var request = require('request');
+request('https://api.meetup.com/Sea-kayakers-friends/events?key=meetup_key_here&sign=true&status=upcoming', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    //console.log(body) // Show the raw response
+    
+    var events = JSON.parse(body);
+    
+    var matchEvent = false;
+    if(events){
+        console.log('upcoming events:');
+        
+        events.map(function(event){
+         
+          if(event.name.toLowerCase().indexOf( 'part b' ) !==-1 || event.name.toLowerCase().indexOf( 'essentials') !==-1 ){
+            matchEvent =true;
+          }
+          if(matchEvent){
+            console.log(event.name, ' *');
+          }else{
+            console.log(event.name);
+          }
+           
+        });
+    }
+    console.log('');
+    console.log('is part b available?: ', matchEvent);
+
+    
+  }
+})
